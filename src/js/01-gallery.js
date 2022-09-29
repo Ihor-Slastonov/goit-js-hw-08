d548aab956fa1e25c7a5615442a2f8e9e4969124
Ihor-Slastonov <jammik000@gmail.com>
2022-09-29 13:52:10 +0300

    first task done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,3 +4,18 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 console.log(SimpleLightbox);
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+const renderImages = galleryItems
+    .map(
+        ({ original, preview, description }) => `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`,
+    )
+    .join('');
+
+galleryRef.innerHTML = '';
+galleryRef.insertAdjacentHTML('beforeend', renderImages);
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
