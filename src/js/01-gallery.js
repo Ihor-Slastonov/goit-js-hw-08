de37972f0ef928f99b80c879d626f81a1e4a2fec
Ihor-Slastonov <jammik000@gmail.com>
2022-09-29 13:47:31 +0300

    add simpleLightBox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,6 @@
+import SimpleLightbox from "simplelightbox";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+console.log(SimpleLightbox);
 console.log(galleryItems);
