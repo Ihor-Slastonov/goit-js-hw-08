47dc39635335382b06448d93ef356286f8f9046f
Ihor-Slastonov <jammik000@gmail.com>
2022-10-01 01:16:11 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,7 +3,6 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-console.log(SimpleLightbox);
 console.log(galleryItems);
 
 const galleryRef = document.querySelector('.gallery');
