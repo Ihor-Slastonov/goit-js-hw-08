344aa4a5c0a952026331071975217129851b4c2f
Ihor-Slastonov <jammik000@gmail.com>
2022-09-30 16:21:26 +0300

    second almost done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Player from "@vimeo/player";
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+
+
+player.on('timeupdate', oncurrentTime)
+
+function oncurrentTime(data) {
+    localStorage.setItem("videoplayer-current-time", data.seconds)
+}
+
+const lastTime = JSON.parse(localStorage.getItem("videoplayer-current-time")) 
+
+player.setCurrentTime(lastTime).then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function (error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
 
