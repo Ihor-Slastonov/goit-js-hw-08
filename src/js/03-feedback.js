328d0b1ca510648164de984c87d4ceca8c151ff8
Ihor-Slastonov <jammik000@gmail.com>
2022-10-02 22:00:24 +0300

    3 task update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,35 +1,32 @@
-import throttle from 'lodash.throttle';
-
-const FORM_KEY_VALUE = 'feedback-form-state';
-
-const form = document.querySelector('.feedback-form');
-
-form.addEventListener('submit', onFormSubmit);
-form.addEventListener('input', throttle(onInput, 500));
-
-populateFormFields();
+const form = document.querySelector('.js-feedback-form');
+const FORM_LOCAL_KEY = 'feedback-form-state';
 const formData = {};
+dataStorage();
+form.addEventListener('input', onFormInput);
+form.addEventListener('submit', onFormSubmit);
 
-function onInput(evt) {
-    formData[evt.target.name] = evt.target.value;
-    localStorage.setItem(FORM_KEY_VALUE, JSON.stringify(formData));
+function onFormInput(e) {
+  formData.email = form.elements.email.value;
+  formData.message = form.elements.message.value;
+  localStorage.setItem(FORM_LOCAL_KEY, JSON.stringify(formData));
+}
 
+function dataStorage() {
+  const localData = JSON.parse(localStorage.getItem(FORM_LOCAL_KEY));
+  if (localData) {
+    form.elements.email.value = localData.email;
+    form.elements.message.value = localData.message;
+  }
 }
 
 function onFormSubmit(e) {
-    e.preventDefault();
-    const sentData = JSON.parse(localStorage.getItem(FORM_KEY_VALUE));
-    if (sentData) {
-        console.log("user-form-data:",sentData)
-    }
-    e.currentTarget.reset();
-    localStorage.removeItem(FORM_KEY_VALUE);
-}
-
-function populateFormFields() {
-    const savedData = JSON.parse(localStorage.getItem(FORM_KEY_VALUE));
-    if (savedData) {
-        form.children[0].firstElementChild.value = savedData.email;
-        form.children[1].firstElementChild.value = savedData.message;
-    }
+  e.preventDefault();
+  const localData = JSON.parse(localStorage.getItem(FORM_LOCAL_KEY));
+  if (localData) {
+    console.log(localData);
+  } else {
+    console.log("FORM FIELDS ARE EMPTY");
+  }
+  e.currentTarget.reset();
+  localStorage.removeItem(FORM_LOCAL_KEY);
 }
